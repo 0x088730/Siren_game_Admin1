d1b5d4d9ae710d8442f0d1d793d10ef79e46c610
waxp2edev0101 <jonathanross0555@gmail.com>
2024-02-05 00:06:09 -0800

    Add presale management

--- src/app/store.tsx
+++ src/app/store.tsx
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import history from './history/historySlice'
 import auth from './auth/authSlice'
+import referData from "./history/referData"
 import {useDispatch} from "react-redux"
 export const store = configureStore({
     reducer: {
         history: history,
+        referData: referData,
         auth: auth
     },
 });
